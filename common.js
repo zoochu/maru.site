c1d1e9ee16c0f13df4c27d7373c80c04e0519836
zoochu <junetldms@naver.com>
2021-08-26 00:21:18 +0900

    08-26

--- common.js
+++ common.js
@@ -1,5 +1,16 @@
 
 $(function(){
-    $('header').load('maru_inc.html header > div',function(){   });
+    $('header').load('maru_inc.html header > div, .menu-trigger',head);
     $('footer').load('maru_inc.html footer > fieldset');
+
+    function head(){
+        
+            const menuTrigger = document.querySelector('.menu-trigger');
+            console.log(menuTrigger)
+
+            menuTrigger.addEventListener('click', function(){
+                this.classList.add('active-2');
+            });
+       
+    }
 });
